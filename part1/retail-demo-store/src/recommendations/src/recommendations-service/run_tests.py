5947d5255661945512fec9e042ed6bd5c4487151
Haowen Huang <51042844+hanyun2019@users.noreply.github.com>
2021-05-22 22:00:49 +0800

    Delete part1 directory

--- part1/retail-demo-store/src/recommendations/src/recommendations-service/run_tests.py
+++
@@ -1,8 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
-
-import os, sys, unittest
-
-sys.argv += ['discover', os.path.dirname(sys.argv[0]), 'test_*.py']
-
-unittest.main(module=None)
